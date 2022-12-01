16042084a53693b0ae5f13894d600fd942a041c6
BryanCevallosCruz <bryancevac@outlook.com>
2022-12-01 11:54:46 -0500

    commit2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter, Route, Routes,Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import ConsumirApi from './componentes/ConsumirApi';
+import Informacion from './componentes/Informacion';
 
 function App() {
   return (
     <>
-    <div>TODO List</div>
-    <ConsumirApi />
+      {/* <div>TODO List</div>
+    <ConsumirApi /> */}
+      <div>TODO List</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ConsumirApi />}>
+
+          </Route>
+          <Route path="*" element={<Informacion/>} />
+        </Routes>
+      </BrowserRouter>
+
     </>
   );
 }
--- src/componentes/ConsumirApi.js
+++ src/componentes/ConsumirApi.js
@@ -1,53 +1,69 @@
 import clientHttp from "../services/ClientHttp";
+import { useNavigate } from "react-router-dom";
 
 const { useEffect, useState } = require("react");
 
 const ConsumirApi = () => {
     const [lista, setLista] = useState([]);
-    //console.log(nombre);
-    useEffect(() => {
-        clientHttp.get(`/`)
-        .then((response)=>{
-            setLista(response.data.data)
-        });
-        // fetch("https://rickandmortyapi.com/api/character")
-        //     .then(response => response.json())
-        //     .then(data => setnombre(data.results))
-        //     .catch(() => console.log("Algo salio mal"))
-    }, []);
-    console.log(lista);
+    const [tarea, setTarea] = useState([]);
+    const [descripcion, setDescripcion] = useState("");
+    const navegacion = useNavigate();
 
-    const handlerCrear=()=>{
-        console.log("Hola")
+    useEffect(() => {
+        clientHttp.get(`/?id_author=18`)
+            .then((response) => {
+                setLista(response.data.data)
+            });
+    }, []);
+
+    const handlerCrear = () => {
+        navegacion(`/*`)
+        clientHttp.post(`/`, tarea)
+            .then(() => {
+                navegacion(`/`)
+            });
     }
 
-    const handlerEliminar=(item)=>{
-        // navegacion(`/`)
-        // clientHttp.delete(`/TipoProducto/?marcaId=${tipo.id}`)
-        //     .then(()=>{
-        //         navegacion(`/tipo-producto`)
-        //     });
+    const handlerEliminar = (item) => {
+        navegacion(`/*`)
+        clientHttp.delete(`/${item.id}`)
+            .then(() => {
+                navegacion(`/`)
+            });
         console.log(item)
     }
+    const handleChange = (event) => {
+        const target = event.target;
+        const value = target.type === "checkbox" ? target.checked : target.value;
+        const name = target.id;
+        setDescripcion((tareaCurrent) => ({ ...tareaCurrent, [name]: value }));
+        console.log(descripcion)
+
+        const fecha = new Date().toISOString();
+        setTarea({ description: descripcion.description, status: 0, id_author: 18, created_at: fecha, finish_at: fecha });
+        console.log(tarea);
+
+    };
 
     return (
         <><table className="table">
-        <thead>
-        <tr>
-            <th ><input type="text" id="description" required maxLength="60"/></th>
-            <th ><button onClick={()=>handlerCrear()}>Agregar</button></th>
-        </tr>
-        </thead>
-        <tbody>
-            {lista.map((item)=>
-                <tr key={item.id}>
-                    <td>{item.description}</td>
-                    <td><button onClick={(e)=>handlerEliminar(item)}>Eliminar</button></td>
-                </tr>)} 
-        </tbody>
-    </table>
-    
-    </>
+            <thead>
+                <tr>
+                    <th ><input type="text" id="description" required maxLength="60"
+                        onChange={e => handleChange(e)} /></th>
+                    <th ><button onClick={() => handlerCrear()}>Agregar</button></th>
+                </tr>
+            </thead>
+            <tbody>
+                {lista.map((item) =>
+                    <tr key={item.id}>
+                        <td>{item.description}</td>
+                        <td><button onClick={(e) => handlerEliminar(item)}>Eliminar</button></td>
+                    </tr>)}
+            </tbody>
+        </table>
+
+        </>
 
     );
 }
